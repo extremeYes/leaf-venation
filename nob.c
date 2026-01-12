9fdcfd27fdf5a54ed7be7ed4e79577285b29eef9
extremeYes <92187320+extremeYes@users.noreply.github.com>
2026-01-12 19:31:12 -0300

    Windows build recipe (MinGW)

--- nob.c
+++ nob.c
@@ -7,6 +7,17 @@ Cmd cmd = {0};
 int main(int argc, char **argv)
 {
     NOB_GO_REBUILD_URSELF(argc, argv);
+#ifdef _WIN32
+    cmd_append(&cmd, "cc");
+    cmd_append(&cmd, "-Wall");
+    cmd_append(&cmd, "-Wextra");
+    cmd_append(&cmd, "-ggdb");
+    cmd_append(&cmd, "-I./raylib-5.5_win64_mingw-w64/include/");
+    cmd_append(&cmd, "-o", "main.exe", "main.c");
+    cmd_append(&cmd, "-L./raylib-5.5_win64_mingw-w64/lib/");
+    cmd_append(&cmd, "-l:libraylib.a");
+    cmd_append(&cmd, "-lgdi32", "-lwinmm");
+#else
     cmd_append(&cmd, "cc");
     cmd_append(&cmd, "-Wall");
     cmd_append(&cmd, "-Wextra");
@@ -16,6 +27,7 @@ int main(int argc, char **argv)
     cmd_append(&cmd, "-L./raylib-5.5_linux_amd64/lib/");
     cmd_append(&cmd, "-l:libraylib.a");
     cmd_append(&cmd, "-lm");
+#endif
     if (!cmd_run(&cmd)) return 1;
     return 0;
 }
